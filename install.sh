b5783f52d68281d6422eae1b4f7c922812db4947
markettv <168855912+markettv@users.noreply.github.com>
2024-05-14 21:48:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,29 @@
-dd
+
+echo "################ UCLAGON-E2-PANEL #################"
+echo "############ KARIM&SAID #################"
+#!/bin/sh
+#
+wget -O /tmp/enigma2-plugin-softcams-ncam_V14.9-r0_all.ipk "https://files.catbox.moe/ep9fes.ipk"
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/UCLAGON-E2-PANEL "https://raw.githubusercontent.com/CASASAT-PANEL/UCLAGON-E2-PANEL/main/__init__.py"
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/UCLAGON-E2-PANEL "https://github.com/CASASAT-PANEL/UCLAGON-E2-PANEL/raw/main/__init__.pyc"
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/UCLAGON-E2-PANEL "https://raw.githubusercontent.com/CASASAT-PANEL/UCLAGON-E2-PANEL/main/plugin.py"
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/UCLAGON-E2-PANEL "https://github.com/CASASAT-PANEL/UCLAGON-E2-PANEL/raw/main/plugin.pyc"
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/UCLAGON-E2-PANEL "https://github.com/CASASAT-PANEL/UCLAGON-E2-PANEL/raw/main/plugin.pyc"
+
+wget -O /usr/lib/enigma2/python/Plugins/Extensions/UCLAGON-E2-PANEL "https://files.catbox.moe/j4pto1.png"
+opkg update && opkg install --force-overwrite /tmp/*.ipk
+
+echo ""
+cd ..
+sync
+echo "############ INSTALLATION COMPLETED ########"
+echo "############ RESTARTING... #################" 
+init 4
+sleep 2
+init 3
+exit 0
